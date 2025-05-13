3d94662e6c36d7af7defc4a9be821e68aa97676e
ELROKA02 <samuelstonecaballero@gmail.com>
2025-05-13 17:25:15 +0200

    He añadido el javadoc de todo

--- src/app/Principal.java
+++ src/app/Principal.java
@@ -2,4 +2,24 @@ package app;
 
 public class Principal {
 
+	/*
+	 * Esta forma numero aleatorio entre 0 y 10
+	 * int aleatorio = (int)Math.random()*10;
+	 */
+	// 1
+	
+	Punto p1 = new Punto (((int)Math.random()*10),((int)Math.random()*10));
+	Punto p2 = new Punto (((int)Math.random()*10),((int)Math.random()*10));
+	Punto p3 = new Punto (((int)Math.random()*10),((int)Math.random()*10));
+	Punto p4 = new Punto (((int)Math.random()*10),((int)Math.random()*10));
+
+
+	// 2
+	
+	Segmento s1 = new Segmento (p1,p2);
+	Segmento s2 = new Segmento (p3,p4);
+	
+	// 3
+	
+
 }
--- src/clases/Segmento.java
+++ src/clases/Segmento.java
@@ -2,4 +2,57 @@ package clases;
 
 public class Segmento {
 
+	/**
+	 * constructor del segmento para darle valores
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public Segmento (Punto a,Punto b) {
+		this.a = a;
+		this.b = b;
+		
+	}
+	
+	/**
+	 * Constructor del segmento sin valores por defecto a=(0,0) y b=(1,1)
+	 * 
+	 */
+	public Segmento () {
+		this.a = new Punto (0,0);
+		this.b = new Punto (1,1);
+		
+	}
+	
+	/**
+	 * metodo para calcular la distancia del segmento
+	 * 
+	 * @return la distancia en decimales
+	 */
+	public double distancia() {
+		this.a
+		double distancia = Math.sqrt(geta())
+	}
+	
+	
+	public void seta (Punto a) {
+		this.a=a;
+	}
+	
+	public void setb (Punto b) {
+		this.b=b;
+	}
+	
+	public double geta() {
+		return this.a;
+	}
+		
+	public double getb() {
+		return this.b;
+	}
+	
+	public String toString() {
+		return "[" + this.a + "," + this.b + "]";
+ 	}
+	
 }
